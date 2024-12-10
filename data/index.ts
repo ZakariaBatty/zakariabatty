491ae8e94ec5acb1ab840b6326c42ca3720fa0bd
ZakariaBatty <zbatty1297@gmail.com>
2024-12-10 17:12:32 +0100

    create 1 file and update 6 files

--- data/index.ts
+++ data/index.ts
@@ -1,217 +1,193 @@
 export const navItems = [
-  { name: "About", link: "#about" },
-  { name: "Projects", link: "#projects" },
-  { name: "Testimonials", link: "#testimonials" },
-  { name: "Contact", link: "#contact" },
+   { name: 'About', link: '#about' },
+   { name: 'Projects', link: '#projects' },
+   { name: 'Testimonials', link: '#testimonials' },
+   { name: 'Contact', link: '#contact' },
 ];
 
 export const gridItems = [
-  {
-    id: 1,
-    title: "I prioritize client collaboration, fostering open communication ",
-    description: "",
-    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
-    imgClassName: "w-full h-full",
-    titleClassName: "justify-end",
-    img: "/b1.svg",
-    spareImg: "",
-  },
-  {
-    id: 2,
-    title: "I'm very flexible with time zone communications",
-    description: "",
-    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
-    imgClassName: "",
-    titleClassName: "justify-start",
-    img: "",
-    spareImg: "",
-  },
-  {
-    id: 3,
-    title: "My tech stack",
-    description: "I constantly try to improve",
-    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
-    imgClassName: "",
-    titleClassName: "justify-center",
-    img: "",
-    spareImg: "",
-  },
-  {
-    id: 4,
-    title: "Tech enthusiast with a passion for development.",
-    description: "",
-    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
-    imgClassName: "",
-    titleClassName: "justify-start",
-    img: "/grid.svg",
-    spareImg: "/b4.svg",
-  },
-
-  {
-    id: 5,
-    title: "Currently building a JS Animation library",
-    description: "The Inside Scoop",
-    className: "md:col-span-3 md:row-span-2",
-    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
-    titleClassName: "justify-center md:justify-start lg:justify-center",
-    img: "/b5.svg",
-    spareImg: "/grid.svg",
-  },
-  {
-    id: 6,
-    title: "Do you want to start a project together?",
-    description: "",
-    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
-    imgClassName: "",
-    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
-    img: "",
-    spareImg: "",
-  },
+   {
+      id: 1,
+      title: 'I specialize in SaaS platform development and API integration.',
+      description:
+         'Delivering scalable and user-focused solutions for various industries.',
+      className: 'lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]',
+      imgClassName: 'w-full h-full',
+      titleClassName: 'justify-end',
+      img: '/b1.svg',
+      spareImg: '',
+   },
+   {
+      id: 2,
+      title: '',
+      description:
+         'Ensuring seamless communication and project delivery worldwide.',
+      className: 'lg:col-span-2 md:col-span-3 md:row-span-2',
+      imgClassName: '',
+      titleClassName: 'justify-start',
+      img: '',
+      spareImg: '',
+   },
+   {
+      id: 3,
+      title: 'My Tech Stack',
+      description: 'Expertise in MERN stack, scalable architecture.',
+      className: 'lg:col-span-2 md:col-span-3 md:row-span-2',
+      imgClassName: '',
+      titleClassName: 'justify-center',
+      img: '',
+      spareImg: '',
+   },
+   {
+      id: 4,
+      title: 'Building future-ready web applications.',
+      description: '',
+      className: 'lg:col-span-2 md:col-span-3 md:row-span-1',
+      imgClassName: '',
+      titleClassName: 'justify-start',
+      img: '/grid.svg',
+      spareImg: '/b4.svg',
+   },
+   {
+      id: 5,
+      title: 'Currently exploring advanced JavaScript frameworks.',
+      description: 'The Inside Scoop',
+      className: 'md:col-span-3 md:row-span-2',
+      imgClassName: 'absolute right-0 bottom-0 md:w-96 w-60',
+      titleClassName: 'justify-center md:justify-start lg:justify-center',
+      img: '/b5.svg',
+      spareImg: '/grid.svg',
+   },
+   {
+      id: 6,
+      title: 'Let’s create something amazing together.',
+      description: '',
+      className: 'lg:col-span-2 md:col-span-3 md:row-span-1',
+      imgClassName: '',
+      titleClassName: 'justify-center md:max-w-full max-w-60 text-center',
+      img: '',
+      spareImg: '',
+   },
 ];
 
 export const projects = [
-  {
-    id: 1,
-    title: "3D Solar System Planets to Explore",
-    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-    img: "/p1.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-    link: "/ui.earth.com",
-  },
-  {
-    id: 2,
-    title: "Yoom - Video Conferencing App",
-    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-    img: "/p2.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-    link: "/ui.yoom.com",
-  },
-  {
-    id: 3,
-    title: "AI Image SaaS - Canva Application",
-    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-    img: "/p3.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-    link: "/ui.aiimg.com",
-  },
-  {
-    id: 4,
-    title: "Animated Apple Iphone 3D Website",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-    img: "/p4.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    link: "/ui.apple.com",
-  },
+   {
+      id: 1,
+      title: 'Dreebz - SaaS Platform for Legal Experts',
+      des: 'Developed a robust platform for legal consultations using React.js, Next.js, and Express.js.',
+      img: '/p1.svg',
+      iconLists: [
+         '/re.svg',
+         '/tail.svg',
+         '/ts.svg',
+         '/express.svg',
+         '/python.svg',
+      ],
+      link: 'http://dreebz.com',
+   },
+   {
+      id: 2,
+      title: 'Sahtek - Telemedicine SaaS Application',
+      des: 'Designed a comprehensive platform for patient-doctor consultations and medical task management.',
+      img: '/p2.svg',
+      iconLists: [
+         '/re.svg',
+         '/tail.svg',
+         '/ts.svg',
+         '/express.svg',
+         '/flask.svg',
+      ],
+      link: '',
+   },
+   {
+      id: 3,
+      title: 'Microservice for Intelligent Search',
+      des: 'Built a search and analysis microservice leveraging Flask and Python for improved accuracy.',
+      img: '/p3.svg',
+      iconLists: ['/python.svg', '/flask.svg', '/tail.svg'],
+      link: '',
+   },
+   {
+      id: 4,
+      title: 'WordPress WooCommerce Store Creation',
+      des: 'Developed feature-rich e-commerce stores and subscription management dashboards.',
+      img: '/p4.svg',
+      iconLists: ['/php.svg', '/wp.svg', '/tail.svg', '/mysql.svg'],
+      link: '',
+   },
 ];
 
 export const testimonials = [
-  {
-    quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
-  },
-  {
-    quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
-  },
-  {
-    quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
-  },
-  {
-    quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
-  },
-  {
-    quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
-  },
+   {
+      quote: "Zakaria's expertise in full-stack development helped us build a scalable and efficient platform. Highly recommended!",
+      name: 'Michael Johnson',
+      title: 'CEO at Red Cursor Solutions',
+   },
+   {
+      quote: 'Zakaria delivered exceptional results on our SaaS project, showing great attention to detail and communication skills.',
+      name: 'Sarah Khalid',
+      title: 'Project Manager at Akwascreation Agency',
+   },
 ];
 
 export const companies = [
-  {
-    id: 1,
-    name: "cloudinary",
-    img: "/cloud.svg",
-    nameImg: "/cloudName.svg",
-  },
-  {
-    id: 2,
-    name: "appwrite",
-    img: "/app.svg",
-    nameImg: "/appName.svg",
-  },
-  {
-    id: 3,
-    name: "HOSTINGER",
-    img: "/host.svg",
-    nameImg: "/hostName.svg",
-  },
-  {
-    id: 4,
-    name: "stream",
-    img: "/s.svg",
-    nameImg: "/streamName.svg",
-  },
-  {
-    id: 5,
-    name: "docker.",
-    img: "/dock.svg",
-    nameImg: "/dockerName.svg",
-  },
+   {
+      id: 1,
+      name: 'Red Cursor Solutions',
+      img: '/redcursor.svg',
+      nameImg: '/redcursorName.svg',
+   },
+   {
+      id: 2,
+      name: 'Akwascreation Agency',
+      img: '/akwa.svg',
+      nameImg: '/akwaName.svg',
+   },
+   {
+      id: 3,
+      name: 'Bfdigital',
+      img: '/bfdigital.svg',
+      nameImg: '/bfdigitalName.svg',
+   },
+   {
+      id: 4,
+      name: 'Ly Assurance',
+      img: '/lyassurance.svg',
+      nameImg: '/lyassuranceName.svg',
+   },
 ];
 
 export const workExperience = [
-  {
-    id: 1,
-    title: "Frontend Engineer Intern",
-    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
-    className: "md:col-span-2",
-    thumbnail: "/exp1.svg",
-  },
-  {
-    id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
-    className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp2.svg",
-  },
-  {
-    id: 3,
-    title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
-    className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp3.svg",
-  },
-  {
-    id: 4,
-    title: "Lead Frontend Developer",
-    desc: "Developed and maintained user-facing features using modern frontend technologies.",
-    className: "md:col-span-2",
-    thumbnail: "/exp4.svg",
-  },
+   {
+      id: 1,
+      title: 'Full-Stack Web Developer',
+      desc: 'Built and optimized SaaS applications and microservices for various industries using modern tech stacks.',
+      className: 'md:col-span-2',
+      thumbnail: '/exp1.svg',
+   },
+   {
+      id: 2,
+      title: 'Freelance Full-Stack Developer',
+      desc: 'Created scalable websites and APIs for clients, ensuring seamless user experiences.',
+      className: 'md:col-span-2',
+      thumbnail: '/exp2.svg',
+   },
 ];
 
 export const socialMedia = [
-  {
-    id: 1,
-    img: "/git.svg",
-  },
-  {
-    id: 2,
-    img: "/twit.svg",
-  },
-  {
-    id: 3,
-    img: "/link.svg",
-  },
+   {
+      id: 1,
+      img: '/git.svg',
+      link: 'https://github.com/zbatty',
+   },
+   {
+      id: 2,
+      img: '/twit.svg',
+      link: 'https://twitter.com/zbatty',
+   },
+   {
+      id: 3,
+      img: '/link.svg',
+      link: 'https://www.linkedin.com/in/zakaria-batty/',
+   },
 ];
